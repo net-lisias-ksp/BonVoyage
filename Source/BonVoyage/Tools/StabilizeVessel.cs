2f03c1100d2e7f3d292e26e9e189a6ed76214b76
Lisias T <me@lisias.net>
2026-05-10 11:59:49 -0300

    Ressurrected Stabilization Code. For https://github.com/net-lisias-ksp/BonVoyage/issues/5

--- Source/BonVoyage/Tools/StabilizeVessel.cs
+++ Source/BonVoyage/Tools/StabilizeVessel.cs
@@ -137,17 +137,16 @@ namespace BonVoyage
         /// </summary>
         private static void Stabilize(Vessel v)
         {
-            // First, we rotate, then move burrowed vessel up and then down if it's too high
-            // We don't need to move down. Stock physics easing can handle it itself.
-            //if (moveVesselUp)
-            //{
-            if (!disableRotation)
-                Rotate(v);
-            MoveUp(v);
-                //moveVesselUp = false;
-            //}
-            //else
-            //    MoveDown(v);
+			// First, we rotate, then move burrowed vessel up and then down if it's too high
+			if (moveVesselUp)
+			{
+				if (!disableRotation)
+					Rotate(v);
+				MoveUp(v);
+				moveVesselUp = false;
+			}
+			else
+				MoveDown(v);
 
             v.ResetGroundContact();
             v.IgnoreGForces(20);
